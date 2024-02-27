02ca313aec870e476f8b21b7fc7073f5b4645c3d
Krzysztof Wojnar <krzysztof.wojnar@capgemini.com>
2024-02-27 23:31:37 +0100

    46: playwright whole page screenshots on windows

--- tests/custom.spec.ts
+++ tests/custom.spec.ts
@@ -1,11 +1,9 @@
 import { test } from '@playwright/test';
-import { describe } from 'node:test';
 
-describe('Our first custom tests', async () => {
-  test('stack overflow', async ({ page, browserName }) => {
+test.describe('Our first custom tests', async () => {
+  test('whole page sceenshot', async ({ page, browserName }) => {
     await page.goto('https://playwright.dev/');
     await page
-      .getByRole('link', {name: 'Stack Overflow'})
-      .screenshot({ animations: 'disabled', path: `stackOverflow${browserName}.png` });
+      .screenshot({ animations: 'disabled', path: `wholePage${browserName}.png`, fullPage: true });
   });
 });
